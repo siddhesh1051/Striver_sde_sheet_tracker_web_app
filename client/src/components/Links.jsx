1bbad45a097332edf9896234b6a260beefeeb67b
siddhesh1051 <siddhesh1052001@gmail.com>
2023-10-13 12:37:34 +0530

    .

--- client/src/components/Links.jsx
+++ client/src/components/Links.jsx
@@ -6,6 +6,20 @@ import WebsiteLogo from "../assets/website.png";
 
 
 const Links = ({ question }) => {
+    function extractVideoId(url) {
+        const videoIdRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+        const match = url.match(videoIdRegex);
+        return match ? match[1] : null;
+    }
+
+    const [isVideoOpen, setIsVideoOpen] = useState(false);
+
+    const handleIconClick = () => {
+      setIsVideoOpen(true);
+    };
+  
+    const videoId = extractVideoId(question?.yt_link);
+    
     return (
         <div style={{ gridColumn: "3", display: 'flex', gap: '20px' }}>
 
@@ -23,24 +37,33 @@ const Links = ({ question }) => {
                     <img src={CodingNinjaLogo} width={30} height={30} alt="" />
                 </a>
             }
-            {
-                question?.yt_link &&
-                <a href={question?.yt_link} target='_blank' style={{ display: 'inline-block', backgroundColor: '#441613', borderRadius: '10px', padding: '8px' }}>
-                    <img src={YoutubeLogo} width={30} height={30} alt="" />
-                </a>
-            }
-            {
-                question?.post_link && (
-                    <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%' }}>
-                        <iframe
-                            title="Embedded Video"
-                            src={question?.post_link}
-                            frameBorder="0"
-                            allowFullScreen
-                            style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
-                        />
-                    </div>
-                )
+            {question?.yt_link && !isVideoOpen && (
+        <a onClick={handleIconClick} style={{ cursor: 'pointer', backgroundColor: '#441613', borderRadius: '10px', padding: '8px', display: 'inline-block' }}>
+          <img src={YoutubeLogo} width={30} height={30} alt="" />
+        </a>
+      )}
+
+      {isVideoOpen && (
+        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.8)', zIndex: 9999, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '80%' }}>
+            <iframe
+              title="Embedded YouTube Video"
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              frameBorder="0"
+              allowFullScreen
+              style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%' }}
+            />
+            <button onClick={() => setIsVideoOpen(false)} style={{ position: 'absolute', top: '10px', right: '10px', backgroundColor: 'transparent', border: 'none', color: '#fff', fontSize: '24px', cursor: 'pointer' }}>X</button>
+          </div>
+        </div>
+      )}
+            {   
+                question?.post_link &&
+                <a href={question?.post_link} target='_blank' style={{ display: 'inline-block', backgroundColor: '#082B40', borderRadius: '10px', padding: '8px' }}>
+                <img src={WebsiteLogo} width={30} height={30} alt="" />
+            </a>
             }
 
         </div>
